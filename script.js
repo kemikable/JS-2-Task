54ccb9cf665855d14d867e42bf2f159f03e3142c
Kamran Aghayev <aghayev.kamran@outlook.com>
2023-02-24 02:35:46 +0400

    0.1

---
+++ script.js
@@ -0,0 +1,17 @@
+let firstName, lastName, fullName;
+firstName="Kamran";
+lastName="Aghayev";
+fullName=firstName + ' ' + lastName;
+console.log(fullName)
+//////
+fullName='Kamran Aghayev';
+console.log(fullName)
+
+///////
+//Bonus?
+
+
+
+let fullName2=prompt("Please input your Name and Surname");
+alert(fullName2.split(' ').reverse().join(' '));
+console.log((fullName2.split(' ').reverse().join(' ')))
